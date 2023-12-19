bc7c3c69cbd605f55b1c9ddaf6d44d3d69817999
yasuyaswa <83386305+yasuyaswa@users.noreply.github.com>
2023-12-19 16:21:56 +0530

    Create MinimumBribes.cs

---
+++ Day4/MinimumBribes.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+ 
+class Result
+{
+ 
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+ 
+    public static void minimumBribes(List<int> q)
+    {
+        int bribes = 0;
+ 
+        for (int i = q.Count - 1; i >= 0; i--)
+        {
+            // Check if the person at position i has moved more than 2 positions forward
+            if (q[i] - (i + 1) > 2)
+            {
+                Console.WriteLine("Too chaotic");
+                return;
+            }
+ 
+            // Count the number of bribes received by the person at position i
+            for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+            {
+                if (q[j] > q[i])
+                {
+                    bribes++;
+                }
+            }
+        }
+ 
+        Console.WriteLine(bribes);
+ 
+    }
+ 
+}
+ 
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+ 
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+ 
+            List<int> q = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(qTemp => Convert.ToInt32(qTemp)).ToList();
+ 
+            Result.minimumBribes(q);
+        }
+    }
+}
