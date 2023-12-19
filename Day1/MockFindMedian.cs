0aebbfd449d2a9da3b4004404248be35a50ce1ee
yasuyaswa <83386305+yasuyaswa@users.noreply.github.com>
2023-12-19 16:11:43 +0530

    Create MockFindMedian.cs

---
+++ Day1/MockFindMedian.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Result
+{
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as a parameter.
+     */
+
+    public static int findMedian(List<int> arr)
+    {
+        int[] arrArray = arr.ToArray();  // Convert List<int> to an array
+        Array.Sort(arrArray);
+        
+        int middleIndex = arrArray.Length / 2;
+        return arrArray[middleIndex];
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.findMedian(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
